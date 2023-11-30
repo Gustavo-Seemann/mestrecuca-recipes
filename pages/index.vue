5af48312def4db30f40fc9949b68176f6dd8833d
Luiz Gustavo S <seemann24@Hotmail.com>
2023-11-29 22:51:33 -0300

    feat: add create famouscategories component

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,10 @@
                 <FeaturedRecipes />
                 <FeaturedRecipes />
             </div>
+            <div class="mb-10">
+                <h2 class="text-2xl ml-3 font-bold text-primary mb-3">Most famous categories</h2>
+                <FamousCategories />
+            </div>
         </div>
     </div>
 </template>
