445f7e1c96aaa4b4ba6a5fa2170e15d412735316
Luiz Gustavo S <seemann24@Hotmail.com>
2023-12-03 21:45:30 -0300

    feat: add categories carousel

---
+++ components/CategoriesCarrousel.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container flex items-center">
+        <div class="prevButton" @click="prevSlide"><Icon name="material-symbols:arrow-left-alt" color="#5B0000" size="50" /></div>
+      <Swiper
+        class="flex items-center justify-center"
+        id="mySlider"
+        :slides-per-view="5"
+        :navigation="{ nextEl: '.nextButton', prevEl: '.prevButton' }"
+        :modules="modules"
+      >
+        <SwiperSlide v-for="categorie in categories">
+          <div class="flex flex-col justify-center items-center">
+            <div class="image-container w-[150px] h-[150px] rounded-full bg-white border-4 border-white overflow-hidden shadow-md hover:shadow-lg mb-3">
+              <img :src="categorie.strCategoryThumb" alt="Imagem da Categoria" style="height: 100%; object-fit: cover;">
+            </div>
+            <h1>{{ categorie.strCategory }}</h1>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div class="nextButton" @click="nextSlide"><Icon name="material-symbols:arrow-right-alt" color="#5B0000" size="50" /></div>
+    </div>
+  </template>
+  
+  <script setup>
+  import useFetchCategories from '~~/composables/useFetchCategories';
+  import { ref } from 'vue';
+  import { Swiper, SwiperSlide} from 'swiper/vue';
+  import { Navigation } from 'swiper/modules';
+  
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  
+  const categories = await useFetchCategories();
+  const modules = ref([Navigation]);
+  </script>
+  
+  <style scoped>
+  /* .swiper-button-prev,
+  .swiper-button-next {
+    width: 50px;
+    background-repeat: no-repeat;
+    &:after {
+      content: 'a';
+    }
+  } */
+  
+  /* Ajuste a posição das flechas conforme necessário */
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,16 @@
                 <FeaturedRecipes />
                 <FeaturedRecipes />
             </div>
-            <div class="mb-10">
+            <div class="mb-16">
                 <h2 class="text-2xl ml-3 font-bold text-primary mb-3">Most famous categories</h2>
                 <FamousCategories />
+            </div>
+            <div>
+                <div class="flex justify-between mb-5">
+                    <h2 class="text-2xl ml-3 font-bold text-primary mb-3">Categories</h2>
+                    <button class="small px-5 bg-primary text-base text-white rounded-lg hover:brightness-150 ease-linear transition-all duration-50">See all categories...</button>
+                </div>
+                <CategoriesCarrousel />
             </div>
         </div>
     </div>
