d064c8205c740455feb504e55d526849f985720e
Luiz Gustavo S <seemann24@Hotmail.com>
2023-12-05 03:52:56 -0300

    refact: fixing some error handling

--- error.vue
+++ error.vue
@@ -13,8 +13,8 @@ const handleError = () => {
     <div>
       <NuxtImg src="/logo.png" />
     </div>
-    <h1 class="text-9xl">{{ error.statusCode }}</h1>
-    <p class="mt-7 text-4xl">{{ error.message }}</p>
+    <h1 class="text-9xl">{{ error?.statusCode }}</h1>
+    <p class="mt-7 text-4xl">{{ error?.message }}</p>
     <button
       @click="handleError"
       class="rounded mt-7 text-2xl bg-primary hover:brightness-200 ease-linear transition-all duration-50 px-7 py-4 text-white"
