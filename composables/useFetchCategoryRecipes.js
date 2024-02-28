18c62fc83cd9d5de6372b44d862bdc48d22cc397
unknown <seemann24@hotmail.com>
2024-02-27 21:39:00 -0300

    fix: https requets

--- composables/useFetchCategoryRecipes.js
+++ composables/useFetchCategoryRecipes.js
@@ -1,5 +1,5 @@
 export default async (category) => {
-    const { data, error } = await useFetch(`http://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+    const { data, error } = await useFetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
 
     if (error.value) {
         throw createError({
